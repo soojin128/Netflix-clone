3dde90638c834e414f3617dddd45ec69f5f8c475
Jenny Kim <kimsujin128@naver.com>
2021-03-13 23:41:43 +0900

    Set Up Environment, Nav

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaPY5OdUjx96CRx3VAzd_7TDVLNWqFT20",
+  authDomain: "netflix-clone-6649c.firebaseapp.com",
+  projectId: "netflix-clone-6649c",
+  storageBucket: "netflix-clone-6649c.appspot.com",
+  messagingSenderId: "800004261381",
+  appId: "1:800004261381:web:8034c9ce86f6f921cbfe6c"
+};
