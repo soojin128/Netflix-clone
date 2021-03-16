7717bcf51836632b2d7b92dc2dffea119c18edb9
Jenny Kim <kimsujin128@naver.com>
2021-03-16 19:43:12 +0900

    implemented Redux

--- src/App.js
+++ src/App.js
@@ -8,21 +8,30 @@ import {
 } from 'react-router-dom';
 import Login from './components/Login';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if(userAuth){
-        console.log(userAuth);
+        dispatch(
+          login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+          })
+        );
       }else{
-        
+        dispatch(logout);
       }
     });
-
+    
     return unsubscribe;
   },[]);
+
   return (
     <div className="app">
       <Router>
@@ -35,7 +44,6 @@ function App() {
           </Route>
         </Switch>
         )}
-        
       </Router>
     </div>
   );
